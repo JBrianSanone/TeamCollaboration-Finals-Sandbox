b56c10d80e3de346a6c22b5c77d0ddac27a11c5b
AlexisBacasen <147580719+AlexisBacasen@users.noreply.github.com>
2023-11-22 10:18:46 +0800

    Update sample.py
    Reverted sample.py source code to be inline with PEP8 standards

--- sample.py
+++ sample.py
@@ -1,59 +1,69 @@
-'''
-This is a collection of random functions that uses different datatypes
-where each function utilizes a different data type. This also includes
-python concepts such as sequential execution.
-'''
-
-def get_student_info(): # Sample Containers - Tuples
+# Sample Containers - Tuples
+def get_student_info():
     name = "March"
     age = 7
     grade = 88.45
-    return name, age, grade
+    return name, age, grade  # returns a tuple
+
 
 print(f"Student Info: {get_student_info()}")
 
-def add_shopping_list(item): # Sample Containers - Lists
-    shoppingList = ["bread", "eggs", "milk", "flour", "baking powder", "sniffing powder",
-                     "rat poison", "gin", "rum", item]
+
+# Sample Containers - Lists
+def add_shopping_list(item):
+    shopping_list = ["bread", "eggs", "milk", item]
     print("The current shopping list is: ", end=" ")
-    print(", ".join(shoppingList))
+    print(", ".join(shopping_list))
+
 
 add_shopping_list("bananas")
 
-def combine_sets(set1, set2): # Sample Containers - Sets
-    unionSet = set1.union(set2)
-    print(f"Union of the first and second set: {unionSet}")
 
-oddSet = {1, 3, 5, 7, 9}
-evenSet = {0, 2, 4, 6, 8}
-print(combine_sets(oddSet, evenSet))
+# Sample Containers - Sets
+def combine_sets(set1, set2):
+    union_set = set1.union(set2)
+    print(f"Union of the first and second set: {union_set}")
 
-def get_game_developers(): # Sample Containers - Dictionaries
-    gameDevelopers = {
-        "Pokemon": "Game Freak", 
+
+odd_set = {1, 3, 5, 7, 9}
+even_set = {0, 2, 4, 6, 8}
+print(combine_sets(odd_set, even_set))
+
+
+# Sample Containers - Dictionaries
+def get_game_developers():
+    game_developers = {
+        "Pokemon": "Game Freak",
         "Minecraft": "Mojang",
         "Honkai: Star Rail": "Hoyoverse",
         "Animal Crossing": "Nintendo",
         "Spider-Man": "Marvel"
     }
 
-    del gameDevelopers["Spider-Man"]
-    return gameDevelopers
+    del game_developers["Spider-Man"]
+    return game_developers
 
-def pretty_print(dictionary): # Iterates the values of game_developer dictionary
+
+def pretty_print(dictionary):
     for key, value in dictionary.items():
         print(f"{key}: {value}")
 
+
 print(pretty_print(get_game_developers()))
 
-def print_order(): # Sample Control Structure - Sequential Execution
+
+# Sample Control Structure - Sequential Execution
+def print_order():
     print("This statement is printed first")
     print("This statement comes second")
     print("This gets to be printed last")
 
+
 print_order()
 
-def check_number(num): # Sample Control Structure - Conditional
+
+# Sample Control Structure - Conditional
+def check_number(num):
     if num is None:
         return 'is None'
     elif num < 0:
@@ -63,11 +73,13 @@ def check_number(num): # Sample Control Structure - Conditional
     else:
         return 'is zero'
 
+
 number = 3
 print(number, check_number(number))
 
 
-def print_winters(): # Sample Control Structure - Iterative While Loop
+# Sample Control Structure - Iterative While Loop
+def print_winters():
     winter = ["December", "January", "February"]
 
     i = 0
@@ -75,23 +87,32 @@ def print_winters(): # Sample Control Structure - Iterative While Loop
         print(winter[i])
         i += 1
 
+
 print_winters()
 
-def find_factorial(num): # Sample Control Structure - Iterative For Loop
+
+# Sample Control Structure - Iterative For Loop
+def find_factorial(num):
     factorial = 1
     for i in range(1, num + 1):
         factorial *= i
     return factorial
 
+
 result = find_factorial(5)
 print(f"The factorial of 5 is: {result}")
 
-def cm_to_inches(cm): # Sample Functions - Converts centimeters to inches
+
+# Sample Functions - Converts centimeters to inches
+def cm_to_inches(cm):
     inches = cm / 2.54
     return inches
 
-def converter(centimeter): # Prints the result of function cm_to_inches
+
+def converter(centimeter):
     inches = cm_to_inches(centimeter)
     print(f"{centimeter} centimeter/s is equal to {inches:.2f} inch/es.")
 
-converter(12)
+
+converter(12)  # calls the converter function and passes the value
+
